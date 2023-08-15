26ba734c00c9d4c53def132a9eaf4d69d805576f
Kentaro-Sakai <1997711kentaro@gmail.com>
2023-08-16 01:47:47 +0900

    add cmake

---
+++ build/CMakeFiles/pathGenerater.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sakaikentarou/pathGenerate/main.cpp" "CMakeFiles/pathGenerater.dir/main.o" "gcc" "CMakeFiles/pathGenerater.dir/main.o.d"
+  "/Users/sakaikentarou/pathGenerate/points.cpp" "CMakeFiles/pathGenerater.dir/points.o" "gcc" "CMakeFiles/pathGenerater.dir/points.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pathGenerater.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathGenerater.dir/main.o"
+  "CMakeFiles/pathGenerater.dir/main.o.d"
+  "CMakeFiles/pathGenerater.dir/points.o"
+  "CMakeFiles/pathGenerater.dir/points.o.d"
+  "pathGenerater"
+  "pathGenerater.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathGenerater.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
